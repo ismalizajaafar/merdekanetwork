3d116c17658ef010851f672f37042c254c18371b
Ismaliza <ismalizajaafar81@gmail.com>
2024-11-21 12:03:31 +0800

    updated cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,21 +14,10 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
+'paths' => ['api/*'],
+'allowed_origins' => ['http://localhost:3000'], // Nuxt.js frontend URL
+'allowed_methods' => ['*'],
+'allowed_headers' => ['*'],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => false,
 
 ];
