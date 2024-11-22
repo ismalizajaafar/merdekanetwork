fc918ab7c5bb9f00f1d32f8a6b21e239c57336bf
Ismaliza <ismalizajaafar81@gmail.com>
2024-11-22 09:34:34 +0800

    updated google map

--- pages/contact.vue
+++ pages/contact.vue
@@ -98,7 +98,7 @@ export default {
           title: "Visit Us",
           description: "Find our office for in-person assistance.",
           buttonText: "Get Directions",
-          link: "https://g.co/kgs/NZm8sWx",
+          link: "https://g.co/kgs/cbmNKU8",
         },
       ],
     };
